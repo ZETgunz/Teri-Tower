4d870b91c5c47c1026a9259537fbd0ba81c9dc13
Frey <95632719+ZETgunz@users.noreply.github.com>
2024-07-09 17:34:33 +0300

    feature update
    added items part 7

--- src/data/items/itemList.js
+++ src/data/items/itemList.js
@@ -48,52 +48,59 @@ var itemList = [
     },
     {
         'type': 'upgrade',
-        'itens': [
+        'items': [
             {
-                'title': 'diamond',
-                'name': 'Diamond',
+                'id': 1,
+                'title': 'refined_mineral',
+                'name': 'Refined Mineral',
                 'rarity': 1,
                 'fuse': null,
                 'forge': 10
             },
             {
-                'title': 'mithril',
-                'name': 'Mithril',
+                'id': 2,
+                'title': 'refined_diamond',
+                'name': 'Refined Diamond',
                 'rarity': 2,
                 'fuse': 10,
                 'forge': 15
             },
             {
-                'title': 'titanium',
-                'name': 'Titanium',
+                'id': 3,
+                'title': 'glacite_jewel',
+                'name': 'Glacite Jewel',
                 'rarity': 3,
                 'fuse': 15,
                 'forge': 20
             },
             {
-                'title': 'glacite_jewel',
-                'name': 'Glacite Jewel',
+                'id': 4,
+                'title': 'glacite',
+                'name': 'Glacite',
                 'rarity': 4,
                 'fuse': 25,
                 'forge': 25
             },
             {
-                'title': 'glacite',
-                'name': 'Glacite',
+                'id': 5,
+                'title': 'refined_mithril',
+                'name': 'Refined Mithril',
                 'rarity': 5,
                 'fuse': 50,
                 'forge': 50
             },
             {
-                'title': 'tungsten',
-                'name': 'Tungsten',
+                'id': 6,
+                'title': 'refined_titanium',
+                'name': 'Refined Titanium',
                 'rarity': 6,
                 'fuse': null,
                 'forge': 100
             },
             {
-                'title': 'umber',
-                'name': 'Umber',
+                'id': 7,
+                'title': 'corleonite',
+                'name': 'Corleonite',
                 'rarity': 7,
                 'fuse': null,
                 'forge': 200
@@ -104,18 +111,21 @@ var itemList = [
         'type': 'level',
         'items': [
             {
-                'title': 'glacite_amalgamation',
-                'name': 'Glacite Amalgamation',
+                'id': 1,
+                'title': 'glacite_shard',
+                'name': 'Glacite Shard',
                 'rarity': 7,
                 'forge': 75
             },
             {
+                'id': 2,
                 'title': 'refined_tungsten',
                 'name': 'Refined Tungsten',
                 'rarity': 7,
                 'forge': 100
             },
             {
+                'id': 3,
                 'title': 'refined_umber',
                 'name': 'Refined Umber',
                 'rarity': 7,
